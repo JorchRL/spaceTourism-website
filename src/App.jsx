ff0b7ef121cea6fea8686bb3e9337cfdd52a36ee
Jorch <jorch.rl@gmail.com>
2021-12-07 12:29:27 -0600

    Add App main component with slider functionality implemented

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+import "./app.scss";
+import Navbar from "./components/Navbar";
+import Home from "./pages/Home";
+import Destinations from "./pages/Destinations";
+import Crew from "./pages/Crew";
+import Technology from "./pages/Technology";
+
+const App = () => {
+  const [currentPage, setCurrentPage] = useState(0);
+
+  const handleScreenChange = (id) => {
+    setCurrentPage(id);
+    console.log(id);
+  };
+  return (
+    <div className='app'>
+      <Navbar handleScreenChange={handleScreenChange} />
+      <div
+        className='mainSlider'
+        style={{
+          transform: `translateX(${-100 * currentPage}vw)`,
+        }}>
+        <Home />
+        <Destinations />
+        <Crew />
+        <Technology />
+      </div>
+    </div>
+  );
+};
+
+export default App;
