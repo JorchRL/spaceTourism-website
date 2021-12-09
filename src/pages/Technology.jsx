a5ba9e330b1f613da3f815232f72c4b07dbddeff
Jorch <jorch.rl@gmail.com>
2021-12-09 14:39:44 -0600

    (WIP) Add Technology page component

--- src/pages/Technology.jsx
+++ src/pages/Technology.jsx
@@ -1,12 +1,94 @@
 import "./technology.scss";
-import React from "react";
+import { useState } from "react";
+import * as data from "../data.json";
+
+//landscape images
+import launchVehicleImg from "../assets/technology/image-launch-vehicle-portrait.jpg";
+import spaceCapsuleImg from "../assets/technology/image-space-capsule-portrait.jpg";
+import spacePortImg from "../assets/technology/image-spaceport-portrait.jpg";
+
+//portrait images
 
 const Technology = () => {
+  const [currentTech, setCurrentTech] = useState(0);
+  const tech = data.technology;
+
+  const handleTechChange = (id) => {
+    setCurrentTech(id);
+  };
   return (
     <div className='technology'>
       <div className='contentContainer'>
+        {/* Title */}
         <div className='title'>
-          <div>Technology</div>
+          <h2>
+            <span>03</span>space launch 101
+          </h2>
+        </div>
+
+        {/* Selector */}
+        <div className='techSelector'>
+          <div
+            className={currentTech === 0 ? "indicator active" : "indicator"}
+            onClick={() => handleTechChange(0)}>
+            <div className='circleBg'></div>
+            <h1 className='num'>1</h1>
+          </div>
+
+          <div
+            className={currentTech === 1 ? "indicator active" : "indicator"}
+            onClick={() => handleTechChange(1)}>
+            <div className='circleBg'></div>
+            <h1 className='num'>2</h1>
+          </div>
+
+          <div
+            className={currentTech === 2 ? "indicator active" : "indicator"}
+            onClick={() => handleTechChange(2)}>
+            <div className='circleBg'></div>
+            <h1 className='num'>3</h1>
+          </div>
+        </div>
+
+        {/* ///////
+        Slider 
+        ////////*/}
+        <div
+          className='techSlider'
+          style={{ transform: `translateX(-${100 * currentTech}vw)` }}>
+          {/* Slide 0 */}
+          <div className={currentTech === 0 ? "techSlide active" : "techSlide"}>
+            <div className='imgContainer'>
+              <img src={launchVehicleImg} alt='' />
+            </div>
+            <div className='infoContainer'>
+              <h2 className='header'>the terminology...</h2>
+              <h2 className='name'>{tech[0].name}</h2>
+              <p className='description'>{tech[0].description}</p>
+            </div>
+          </div>
+          {/* Slide 1 */}
+          <div className={currentTech === 0 ? "techSlide active" : "techSlide"}>
+            <div className='imgContainer'>
+              <img src={spacePortImg} alt='' />
+            </div>
+            <div className='infoContainer'>
+              <h2 className='header'>the terminology...</h2>
+              <h2 className='name'>{tech[1].name}</h2>
+              <p className='description'>{tech[1].description}</p>
+            </div>
+          </div>
+          {/* Slide 2 */}
+          <div className={currentTech === 0 ? "techSlide active" : "techSlide"}>
+            <div className='imgContainer'>
+              <img src={spaceCapsuleImg} alt='' />
+            </div>
+            <div className='infoContainer'>
+              <h2 className='header'>the terminology...</h2>
+              <h2 className='name'>{tech[2].name}</h2>
+              <p className='description'>{tech[2].description}</p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
